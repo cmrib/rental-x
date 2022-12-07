c23a8cbee9a355f3abdc3d3ba7b07354bcb316aa
cmrib <ciceromirandar@gmail.com>
2022-12-07 08:18:53 -0300

    create specifications route

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,24 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("Todo");
+  constructor(private specificationsRepository: ISpecificationsRepository) {
+    console.log("Specification");
+  }
+
+  execute({ name, description }: IRequest) {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationsRepository.create({ name, description });
   }
 }
 
